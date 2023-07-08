4b666f6fa39d0eaccda640883bbd47aee4a7039c
miguel gutierrez torres <miguel@tailorbird.us>
2023-07-07 19:45:19 -0500

    semana 06

---
+++ semana05/dia03-portafolio/React/miportafolio/src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import { useEffect } from "react";
+
+function App() {
+  return <h1 className="text-indigo-600 text-2xl font-[500]">Hello</h1>;
+}
+
+export default App;
---
+++ semana06/dia02-javascript/index.js
@@ -0,0 +1,1 @@
+let word1 = "de";
